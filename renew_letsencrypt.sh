56e3bc49835e54a1709289dfa1cc55a2d45bbc16
NoDesireMonk <chinglish@gmail.com>
2016-06-26 23:27:45 -0500

    Update renew_letsencrypt.sh

--- renew_letsencrypt.sh
+++ renew_letsencrypt.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
-cd /opt/letsencrypt/
-./letsencrypt-auto --config /etc/letsencrypt/configs/my-domain.conf certonly
-
-if [ $? -ne 0 ]
- then
-  ERRORLOG=`tail /var/log/letsencrypt/letsencrypt.log`
-  echo -e "The Let's Encrypt cert has not been renewed! \n \n" \
-  $ERRORLOG
- else
-  nginx -s reload
+if [[ $1 ]]
+then
+    CONF=/etc/letsencrypt/configs/$1.conf
+    if [[ -f $CONF ]]
+    then
+        cd /opt/letsencrypt/
+        ./letsencrypt-auto --config $CONF certonly
+        if [ $? -ne 0 ]
+        then
+            ERRORLOG=`tail /var/log/letsencrypt/letsencrypt.log`
+            echo -e "The Let's Encrypt cert has not been renewed! \n \n" \
+                $ERRORLOG
+        else
+            nginx -s reload
+            echo -e "The Let's Encrypt cert has been successfully renewed! \n \n"
+        fi
+    else
+        die "config file is not found"
+    fi
+else
+    echo ' '
+    echo 'Usage:' `basename $0` '[site]'
+    echo 'Please specify the site name'
 fi
 
 exit 0
