c55532f8443fcce58002f19054a67b1ff8ed4130
NoDesireMonk <chinglish@gmail.com>
2017-01-14 19:23:54 -0600

    Update vadmin.sh

--- vadmin.sh
+++ vadmin.sh
@@ -328,7 +328,7 @@ function install_php {
 	check_install php5-cli php5-cli
 
 	# PHP modules
-	DEBIAN_FRONTEND=noninteractive apt-get -y install php5-cgi php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-mhash php5-pspell php5-recode php5-snmp php5-tidy php5-xmlrpc php-gettext php5-xsl php5-common php5-cli php5-fpm php5-apcu php5-dev php5-mysql php5-sqlite
+	DEBIAN_FRONTEND=noninteractive apt-get -y install php5-cgi php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-mhash php5-pspell php5-recode php5-tidy php5-xmlrpc php-gettext php5-xsl php5-common php5-cli php5-fpm php5-apcu php5-dev php5-mysql php5-sqlite
 
 	echo 'Using PHP-FPM to manage PHP processes'
 	echo ' '
