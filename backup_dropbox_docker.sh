d4ea243cf0f13e8ce74a3e02c12e86395ee424c4
NoDesireMonk <chinglish@gmail.com>
2021-04-09 22:24:04 -0500

    Update backup_dropbox_docker.sh

--- backup_dropbox_docker.sh
+++ backup_dropbox_docker.sh
@@ -13,7 +13,7 @@ NOW=$(date +"%A")
 DESTFILE="$BACKUP_DST/$NOW.tar.gz"
 
 /usr/bin/docker exec $MYSQL_CONTAINER /usr/bin/mysqldump -u $MYSQL_USER -p$MYSQL_PASS $MYSQL_DB > "$NOW-Databases.sql"
-tar --exclude-vcs -zcvf "$DESTFILE" $BACKUP_SRC "$NOW-Databases.sql"
+tar --exclude-vcs -zcf "$DESTFILE" $BACKUP_SRC "$NOW-Databases.sql"
 
 $SCRIPT_DIR/dropbox_uploader.sh upload "$DESTFILE" "$DROPBOX_DIR/$NOW.tar.gz"
 
