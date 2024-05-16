9dcff582901568c59a307da084cb313403521091
NoDesireMonk <1267069+nodesiremonk@users.noreply.github.com>
2024-05-16 16:07:11 -0500

    Update backup_rclone_docker.sh

--- backup_rclone_docker.sh
+++ backup_rclone_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SCRIPT_DIR="/root/backup/rclone"
+RCLONE="/root/backup/rclone/rclone"
+DOCKER="/usr/bin/docker"
 REMOTE_NAME="rclone-remote-name"
 REMOTE_DIR="/pat/to/remote/folder"
 BACKUP_SRC="/path/to/backup"
@@ -11,12 +12,16 @@ MYSQL_DB="--all-databases"
 MYSQL_CONTAINER="mysql"
 KEEP_COPY=10
 
-NOW=$(($(date +"%j") % $KEEP_COPY))
-DESTFILE="$BACKUP_DST/$NOW.tar.gz"
+#ADD=$(($(date +"%j") % $KEEP_COPY))
+ADD=$(date +"%Y-%m-%d")
+DEL=$(date --date="$KEEP_COPY day ago" +"%Y-%m-%d")
+DEST_FILE="$BACKUP_DST/$ADD.tar.gz"
+DB_FILE="$ADD-Databases.sql"
 
-/usr/bin/docker exec $MYSQL_CONTAINER /usr/bin/mysqldump --no-tablespaces -u $MYSQL_USER -p$MYSQL_PASS $MYSQL_DB > "$NOW-Databases.sql"
-tar --exclude-vcs --exclude "node_modules" --exclude "tmp" --exclude "_static" -zcf "$DESTFILE" $BACKUP_SRC "$NOW-Databases.sql"
+$DOCKER exec $MYSQL_CONTAINER /usr/bin/mysqldump --no-tablespaces -u $MYSQL_USER -p$MYSQL_PASS $MYSQL_DB > $DB_FILE
+tar --exclude-vcs --exclude "node_modules" --exclude "tmp" -zcf $DEST_FILE $BACKUP_SRC $DB_FILE
 
-$SCRIPT_DIR/rclone copyto --onedrive-no-versions "$DESTFILE" "$REMOTE_NAME:$REMOTE_DIR/$NOW.tar.gz"
+$RCLONE copyto --onedrive-no-versions $DEST_FILE "$REMOTE_NAME:$REMOTE_DIR/$ADD.tar.gz"
+$RCLONE deletefile "$REMOTE_NAME:$REMOTE_DIR/$DEL.tar.gz"
 
-rm -f "$NOW-Databases.sql" "$DESTFILE"
+rm -f $DB_FILE $DEST_FILE
