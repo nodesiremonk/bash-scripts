1b077345c02bbb86e596a2c8c87f6ed85117df9f
NoDesireMonk <1267069+nodesiremonk@users.noreply.github.com>
2024-05-16 12:37:14 -0500

    Update backup_rclone_docker.sh

--- backup_rclone_docker.sh
+++ backup_rclone_docker.sh
@@ -9,13 +9,14 @@ MYSQL_USER="root"
 MYSQL_PASS="rootpassword"
 MYSQL_DB="--all-databases"
 MYSQL_CONTAINER="mysql"
+KEEP_COPY=10
 
-NOW=$(date +"%A")
+NOW=$(($(date +"%j") % $KEEP_COPY))
 DESTFILE="$BACKUP_DST/$NOW.tar.gz"
 
 /usr/bin/docker exec $MYSQL_CONTAINER /usr/bin/mysqldump --no-tablespaces -u $MYSQL_USER -p$MYSQL_PASS $MYSQL_DB > "$NOW-Databases.sql"
 tar --exclude-vcs --exclude "node_modules" --exclude "tmp" --exclude "_static" -zcf "$DESTFILE" $BACKUP_SRC "$NOW-Databases.sql"
 
-$SCRIPT_DIR/rclone copyto "$DESTFILE" "$REMOTE_NAME:$REMOTE_DIR/$NOW.tar.gz"
+$SCRIPT_DIR/rclone copyto --onedrive-no-versions "$DESTFILE" "$REMOTE_NAME:$REMOTE_DIR/$NOW.tar.gz"
 
 rm -f "$NOW-Databases.sql" "$DESTFILE"
